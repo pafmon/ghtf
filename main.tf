e4d000ff8700c54a700957e9b5ab12b706b7d02b
pafmon <pafmon@gmail.com>
2020-09-17 11:56:42 +0200

    check

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "testing" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
-    Name = "terraform-example"
+    Name = "terraform-testing"
   }
 }
 
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "terraform-testing-instance"
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -23,6 +23,6 @@ resource "aws_security_group" "instance" {
 }
 
 output "public_ip" {
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.testing.public_ip
   description = "The public IP of the web server"
 }
